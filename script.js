4513c6ebb957954f6882a296ee587d33cb6613a2
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-12 12:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) 
-    return key in window;
-}
-
+function hasKey(key) {
+    return key in sampleObject;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key));
+alert(hasKey(key)
